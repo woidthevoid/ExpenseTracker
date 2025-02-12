2a5f1c633630514357922826c8e5df0cbf017404
Victor Dralle <woydowskidralle@gmail.com>
2025-02-12 22:35:42 +0100

    Added list view of expenses, methods to delete and fetch expenses, api delete method

--- expense_tracker/lib/screens/home_screen.dart
+++ expense_tracker/lib/screens/home_screen.dart
@@ -1,10 +1,74 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/model/expense.dart';
+import 'package:expense_tracker/services/api.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Expense> expenses = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future<void> fetchExpenses() async {
+      try {
+        final fetchedExpenses = await APIService().fetchExpenses();
+        setState(() {
+          expenses = fetchedExpenses;
+        });
+      } catch (e) {
+        print('Failed to fetch expenses: $e');
+      }
+    }
+  
+    @override
+    void initState() {
+      super.initState();
+      fetchExpenses();
+    }
+  }
+
+  void deleteExpense(String id) {
+    setState(() {
+      expenses.removeWhere((expense) => expense.id == id);
+      APIService().deleteExpense(id);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Hi there'),
+    return Scaffold(
+      appBar: AppBar(title: Text('My Expenses')),
+      body: expenses.isEmpty 
+      ? Center(child: CircularProgressIndicator())
+      : ListView.builder(
+        itemCount: expenses.length,
+        itemBuilder: (context, index) {
+          final expense = expenses[index];
+          return Dismissible(
+            key: Key(expense.id),
+            direction: DismissDirection.endToStart,
+            background: Container(
+              color: Colors.red,
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Icon(Icons.delete, color: Colors.white,),
+            ),
+            onDismissed: (direction) => deleteExpense(expense.id),
+            child: ListTile(
+              title: Text(expense.title),
+              subtitle: Text('${expense.amount} DKK'),
+              trailing: Text(expense.category),
+            )
+            );
+        }
+      )
     );
   }
 }
--- expense_tracker/lib/services/api.dart
+++ expense_tracker/lib/services/api.dart
@@ -5,14 +5,22 @@ import '../model/expense.dart';
 class APIService {
   static const String _url = 'http://localhost:5001';
 
-  Future<Expense> fetchExpenses() async {
+  Future<List<Expense>> fetchExpenses() async {
     final response = await http.get(Uri.parse('$_url/expenses'));
 
     if (response.statusCode == 200) {
-      return Expense.fromJson(
-          jsonDecode(response.body) as Map<String, dynamic>);
+      final List<dynamic> jsonResponse = jsonDecode(response.body) as List<dynamic>;
+      return jsonResponse.map((json) => Expense.fromJson(json as Map<String, dynamic>)).toList();
     } else {
       throw Exception('Failed to fetch expenses');
+    }
+  }
+
+  Future<void> deleteExpense(String id) async {
+    final response = await http.delete(Uri.parse('$_url/expenses/$id'));
+
+    if(response.statusCode != 200) {
+      throw Exception('Failed to delete expense');
     }
   }
 }
